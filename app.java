93d89688808dbdac2def53ab16a915bfff4e5116
sufiyan440 <76608895+sufiyan440@users.noreply.github.com>
2021-02-02 15:49:08 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,10 +1,10 @@
 public class SumOfArray {  
-    public static void main(String[] args) {  
+    public static void main(Saaaa[] args) {  
         //Initialize array  
         int [] arr = new int [] {1, 2, 3, 4, 5};  
         int sum = 0;  
-        //Loop through the array to calculate sum of elements  
-        for (int i = 0; i < arr.length; i++) {  
+        //Loop through the araacwd to calculate sum of elements  
+        for (int i = 0; i < qcsvdvvvngth; i++) {  
            sum = sum + arr[i];  
         }  
         System.out.println("Sum of alhcvavchcjhvhjccvhjbcbhbchbcvhcvhcarray: " + sum);  
