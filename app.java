f6e3aa804771e010538b0d73dea4d929679091ba
sufiyan <sufiyan440@gmail.com>
2021-02-02 15:26:03 +0530

    2modified

--- app.java
+++ app.java
@@ -1,42 +1,12 @@
-public class Sub_Matrix  
-{  
+public class SumOfArray {  
     public static void main(String[] args) {  
-        int rows, cols;  
-  
-        //Initialize matrix a  
-          int a[][] = {  
-                          {4, 5, 6},  
-                          {3, 4, 1},  
-                          {1, 2, 3}  
-                       };  
-  
-          //Initialize matrix b  
-          int b[][] = {  
-                          {2, 0, 3},  
-                         {2, 3, 1},  
-                         {1, 1, 1}  
-                     };  
-  
-          //Calculates number of rows and columns present in given matrix  
-          rows = a.length;  
-        cols = a[0].length;  
-  
-          //Array diff will hold the result  
-        int diff[][] = new int[rows][cols];  
-  
-        //Performs subtraction of matrices a and b. Store the result in matrix diff  
-        for(int i = 0; i < rows; i++){  
-            for(int j = 0; j < cols; j++){  
-                diff[i][j] = a[i][j] - b[i][j];  
-            }  
+        //Initialize array  
+        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        int sum = 0;  
+        //Loop through the array to calculate sum of elements  
+        for (int i = 0; i < arr.length; i++) {  
+           sum = sum + arr[i];  
         }  
-  
-        System.out.println("Subtraction of two matrices: ");  
-        for(int i = 0; i < rows; i++){  
-            for(int j = 0; j < cols; j++){  
-               System.out.print(diff[i][j] + " ");  
-            }  
-            System.out.println();  
-        }  
+        System.out.println("Sum of all the elements of an array: " + sum);  
     }  
-}
+} [A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[D[C[ Vvhdagchdjhhn  hchb [B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
